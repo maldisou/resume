1ec222f68f7e7433d98522f01cca517e98aeb200
maldisou42 <maldisou@student.42abudhabi.ae>
2022-12-24 16:03:45 +0400

    d

---
+++ build/precache-manifest.6545b9021bae81911f1d911a8adcb80d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f41fabfa913f01c5b7fe",
+    "url": "/react-resume/static/css/main.3ed59f80.chunk.css"
+  },
+  {
+    "revision": "f41fabfa913f01c5b7fe",
+    "url": "/react-resume/static/js/main.6ed784cd.chunk.js"
+  },
+  {
+    "revision": "4bccf1df4c11c83de082",
+    "url": "/react-resume/static/js/runtime~main.e0604a19.js"
+  },
+  {
+    "revision": "f827b8256aac030d5c34",
+    "url": "/react-resume/static/js/2.0c57601a.chunk.js"
+  },
+  {
+    "revision": "7c93dca1ef29f7fd080a",
+    "url": "/react-resume/static/js/3.eb6fb1c8.chunk.js"
+  },
+  {
+    "revision": "c2062b7215b5debd59cf3995b61f02a6",
+    "url": "/react-resume/index.html"
+  }
+];
---
+++ src/Schemas/Data.js
@@ -0,0 +1,145 @@
+export const Data = {
+  profile: {
+    name: "Balsam Mohammed",
+    ocupation: "General Practitioner ",
+    location: "United Arab  Emirates, Abu Dhabi",
+    email: "balsamdisougi@gmail.com",
+    telephone: "+971 552392024",
+    image: "images/pp.jpeg",
+  },
+  aboutMe: {
+    label: "Profile",
+    description:
+      "General practitioner with over 10   years of attending experience. Family Medicine experience. Comprehensive understanding of medical treatment within a managed care organization. Clinician with strong people skills, bedside manner, and patient communication skills gained through more than five years of combined education and experience Previous positions have included work in both emergency departments and independent clinics.",
+  },
+  skills: {
+    technicalLabel: "Medical licenses",
+    softLabel: "Skills",
+    technicalSkills: [
+      "Department of Health Abu Dhabi DOH / HAAD registration (HPPRF-2020-001209) (Eligibility letter October 2020",
+      "Full registration in Sudan Medical Council No. 48593 | Jun. 2016",
+     ],
+    softSkills: [
+      "communication & interpersonal skills",
+      "professionalism & humanism",
+      "attentive to details",
+      "skillfull negotiation of the healthcare system",
+      "Critical Thinkning",
+      "scholary approch to clinical practice",
+    ],
+  },
+  socialMedia: {
+    label: "",
+    social: [
+      // {
+      //   label: "clincal ",
+      //   name: "linkedin",
+      //   url: "https://www.linkedin.com/in/mohammed-disougi-9427a9135/",
+      //   className: "bxl-linkedin-square",
+      // },
+      // {
+      //   label: "Maldisou",
+      //   name: "Gmail",
+      //   url: "https://mohammedaldisougi@gmail.com/",
+      //   className: "bxl-gmail",
+      // },
+    ],
+  },
+
+  experience: {
+    works: [
+      {
+        title: "Somerian Health care | UAE - Abu Dhabi",
+        period: "Sep. 2020 - current ",
+        company: "General Practitioner",
+        description: [
+          ""
+        ],
+      },
+      {
+        title: "Almehwar Modren Medical Complex | Sudan",
+        period: "Sep. 2018 - Dec. 2019 ",
+        company: "General Practitioner",
+        description: [
+          ""
+        ],
+      },
+      {
+        title: "Omdurman teaching Hospital | Sudan",
+        period: "Sep. 2016 - Aug. 2018 ",
+        company: "General Practitioner",
+        description: [
+          ""
+        ],
+      },
+      {
+        title: "Almanar Voluntary Organization | Sudan",
+        period: "Oct. 2015 - May. 2018 ",
+        company: "General Practitioner",
+        description: [
+          ""
+        ],
+      },
+      {
+        title: "Federal Minsitry of Health | Sudan",
+        period: "jan. 2014 - feb. 2016 ",
+        company: "House Officer",
+        description: [
+          ""
+        ],
+      },
+     
+     
+    ],
+    academic: [
+      {
+        career: "Master degree in family medicine | Egypt ",
+        date: "Apr. 2021 - ongoing (1st year finsihed)",
+        institution: "Bani Suef University",
+      },
+      {
+        career: "Medicine and bachelor of Surgery (MBBS) | Madani, Sudan",
+        date: "Jun. 2014",
+        institution: "University of Gezira Faculty of Medicine",
+      },
+
+    ],
+    proyects: [
+      {
+        name: "BLS",
+        company: "Cleveland Hospital | UAE, Abu Dhabi",
+        period: "sep. 2021",
+        description: [
+          "",
+        ],
+      },
+      {
+        name: "Emergency & Trauma",
+        company: "SKMC Hospital | UAE, Abu Dhabi",
+        period: "Aug. 2016",
+        description: [
+          "",
+        ],
+      },
+      {
+        name: "Emergency & Trauma",
+        company: "Ibrahim Malik Hospital | Sudan, Khartoum",
+        period: "Aug. 2016",
+        description: [
+          "",
+        ],
+      },
+      
+    ],
+    proyects1: [
+      {
+        name: "BLS",
+        company: "Cleveland Hospital | UAE, Abu Dhabi",
+        period: "sep. 2021",
+        description: [
+          "",
+        ],
+      },
+    ]
+  },
+};
