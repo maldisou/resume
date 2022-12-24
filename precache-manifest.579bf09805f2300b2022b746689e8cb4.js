04c0801ca9b537b39e1e5b0171f89ecc100eaacc
maldisou42 <maldisou@student.42abudhabi.ae>
2022-12-24 16:23:45 +0400

    Updates

---
+++ precache-manifest.579bf09805f2300b2022b746689e8cb4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fcf9586fb02b8630bbcb",
+    "url": "/resume/static/css/main.7c7a9c28.chunk.css"
+  },
+  {
+    "revision": "fcf9586fb02b8630bbcb",
+    "url": "/resume/static/js/main.b2e401bb.chunk.js"
+  },
+  {
+    "revision": "075c34b5d4d37640f880",
+    "url": "/resume/static/js/runtime~main.a7b0534c.js"
+  },
+  {
+    "revision": "b626ab745d585e54291a",
+    "url": "/resume/static/js/2.ac260b5f.chunk.js"
+  },
+  {
+    "revision": "7c93dca1ef29f7fd080a",
+    "url": "/resume/static/js/3.eb6fb1c8.chunk.js"
+  },
+  {
+    "revision": "936650653b7479d198856cb760895dc1",
+    "url": "/resume/index.html"
+  }
+];
